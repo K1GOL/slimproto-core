9dc7a25c0fd1220ed84240c2fb3ff9ba74281d70
K1GOL <42120370+K1GOL@users.noreply.github.com>
2024-06-02 16:59:18 +0300

    Version 0.2.0

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
 # slimproto-core
 
-#### Peer-to-peer identity management protocol.
+### Peer-to-peer identity management protocol.
 
-With Slim, peers can verify each other's identities without a central identity authority. A user creates an _identity ticket_ with a public name and a private password. From these an Ed25519 key pair is generated, which is used to identify the user.
+With _Slim_, peers can verify each other's identities without a central identity authority. Keys are derived from credentials that are easy to store and even remember.
 
----
+#### Contents
 
-# Usage
+- [Description](https://github.com/K1GOL/slimproto-core#description)
+- [Documentation](https://github.com/K1GOL/slimproto-core#documentation)
+- [Testing](https://github.com/K1GOL/slimproto-core#testing)
+- [Example usage](https://github.com/K1GOL/slimproto-core#example-usage)
 
-## createKeyPair(name, password)
+## Description
 
-Creates an Ed25519 key pair for a given name and password.
+The Slim protocol is a peer-to-peer identity management protocol that allows peers to verify each other's identities without a central identity authority.
+
+Each user creates an identity ticket with a public name and a private password. From these, an Ed25519 key pair is generated, which is used to identify the user.
+
+For verification of the identity of a peer, the peer presents an identity ticket. The identity ticket containins the following information:
+
+- Name length (2 bytes): The length of the user's name in bytes.
+- Name (n <= 65535 bytes): The user's name.
+- Public key (32 bytes): The user's public key.
+- Challenge answer (64 bytes): The answer to a random challenge signed with the user's private key.
+- Signature for preceding content (64 bytes): The signature of the preceding content (name length, name, public key, challenge answer) signed with the user's private key.
+  
+To verify the identity ticket, the recipient compares the ticket's signature and challenge answer against the ticket's public key to determine if the ticket is valid.
+
+The Slim protocol is designed to be simple and easy to use, with keys derived from user credentials that are easy to store and even remember by humans. It is designed for applications that require peer-to-peer identity management without a central authority.
+
+## Documentation
+
+### slimprotoVersion
+
+Returns the version of the protocol that is implemented.
+
+### createKeyPair(name, password)
+
+Generates an Ed25519 key pair for the given name and password. The key pair is returned as a buffer containing the concatenated private key (32 bytes) and public key (32 bytes), totaling 64 bytes.
 
 Parameters:
 <dl>
@@ -22,13 +49,21 @@ Parameters:
 
 Returns:
 
-```js
-{ privateKey, publicKey }
-```
+Buffer containing concatenated private key (32 bytes) and public key (32 bytes), total 64 bytes.
 
-## createIdentityTicket(name, password, challenge)
+### keyPairToObject(keyPair)
 
-Creates a Slim identity ticket for a given name and password, identifying this user. A challenge consisting of a sequence of random bytes is provided by the other party and signed with this user's private key. The entire ticket is then signed as well.
+Converts a buffer containing concatenated public and private keys (generated with `createKeyPair`) into an object with separate `publicKey` and `privateKey` properties.
+
+### createIdentityTicket(name, password, challenge)
+
+Generates a Slim identity ticket for a given name, password, and challenge. The ticket is returned as a buffer containing the following information:
+
+- Name length (2 bytes): The length of the user's name in bytes.
+- Name (n <= 65535 bytes): The user's name.
+- Public key (32 bytes): The user's public key.
+- Challenge answer (64 bytes): The answer to the challenge signed with the user's private key.
+- Signature for preceding content (64 bytes): The signature of the preceding content (name length, name, public key, challenge answer) signed with the user's private key.
 
 Parameters:
 <dl>
@@ -42,18 +77,15 @@ Parameters:
 
 Returns:
 
-```js
-ticket = {
-    name,
-    publicKey,
-    challengeAnswer,
-    signature
-  }
-```
+Identity ticket, a concatenated buffer containing specified information.
 
-## verifyIdentityTicket(ticket, challengeQuestion)
+### ticketToObject
 
-Verifies a given Slim identity ticket by comparing the ticket's signature and challenge answer against the ticket's public key.
+Converts a buffer ticket with concatenated data to an object with a appropriate properties: `nameLength`, `name`, `publicKey`, `challengeAnswer`, `signature`.
+
+### verifyIdentityTicket(ticket, challengeQuestion)
+
+Verifies a given identity ticket by comparing the ticket's signature and challenge answer against the ticket's public key.
 
 Parameters:
 <dl>
@@ -67,7 +99,7 @@ Returns:
 
 `true` if the ticket is valid, `false` if not.
 
-## solveChallenge(challenge, privateKey)
+### solveChallenge(challenge, privateKey)
 
 Solves a given challenge by signing it with the private key.
 
@@ -83,27 +115,42 @@ Returns:
 
 Base64 string answer.
 
----
+### generateRandomChallenge(*[length]*)
 
+Generates `length` bytes of random data to be used as a challenge. `length` is optional and default is `512`.
+
+## Testing
+
+A simple testing script is included. It generates a test ticket and verifies it. A successful test should end with the final ticket verification returning `true`. Run with `npm test`.
+
+## Example usage
+
+User A:
 ```js
 // User A wishes to verify the identity of User B.
 // User A creates a challenge.
-const challenge = Buffer.from([1, 2, 3, 4])
+const challenge = generateRandomChallenge()
 
 //
 // This challenge is then sent to User B.
 //
+```
 
+User B:
+```js
 // User B creates an identity ticket with their credentials and the provided challenge.
 const ticketB = createIdentityTicket('UserB', 'Wordpass', challenge)
-// => { name: 'UserB', publicKey: 'jAlpP1nEJ+/y6A6D...
+// => <Buffer 05 00 55 73 65 72 42 59 d7 66 d9 19 cb 8f 74 43 59 d3 45 36 b9 0f d5 2a 68 64 4a 13 4e 94 e0 73 8e b7 30 34 04 14 27 7f 06 6b 40 88 da 93 dd 1b 28 c2 ... 117 more bytes>
 
 //
 // This ticket is then sent to User A.
 //
+```
 
+User A :
+```js
 // User A verifies the ticket.
 verifyIdentityTicket(ticketB, challenge)
 // => true
-// User A now agrees User B is who they claim to be, "UserB" with the public key "jAlpP1nEJ+/y6A6D..."
+// User A now agrees User B is who they claim to be, "UserB" with the public key <Buffer 59 d7 66 d9 19 cb 8f 74 43 59 d3 45 36 b9 0f d5 2a 68 64 4a 13 4e 94 e0 73 8e b7 30 34 04 14 27>
 ```
--- index.js
+++ index.js
@@ -1,73 +1,145 @@
 import pkg from 'node-forge'
 import { pbkdf2Sync } from 'pbkdf2'
-const { pki, md } = pkg
+const { pki, md, random, util } = pkg
 
+// Version of protocol implemented.
+export const slimprotoVersion = '0.2'
+
+// Creates a key pair for a given name and password.
+// Return format (bytes):
+// [Private key, 32 bytes][Public key, 32 bytes]
+// Length 64 bytes.
 export const createKeyPair = (name, pwd) => {
-  const nHash = md.sha256.create().update(name, 'utf8').digest().getBytes().toString('base64')
-  const pHash = md.sha256.create().update(pwd, 'utf8').digest().getBytes().toString('base64')
-  const derivedKey = pbkdf2Sync(nHash + pHash, nHash, 4096, 32, 'sha512')
-  const seed = derivedKey
-  const { privateKey, publicKey } = pki.ed25519.generateKeyPair({ seed })
-  return { privateKey, publicKey }
+  // Trim name to max 65535 bytes.
+  const nameBytes = Buffer.from(name, 'utf8').subarray(0, 65535)
+  // Hash for name.
+  const nHash = Buffer.from(md.sha256.create().update(nameBytes.toString('utf8'), 'utf8').digest().toHex(), 'hex')
+  // Hash for password.
+  const pHash = Buffer.from(md.sha256.create().update(pwd, 'utf8').digest().toHex(), 'hex')
+
+  // Calculate derived key.
+  // Input is (bytes): [nHash][pHash]
+  // Salt is (bytes): [nHash]
+  // Iterations is: 4069
+  // Key length is: 64 byte
+  // Hash algorithm is: SHA-256
+  const pbkdfPassword = Buffer.concat([nHash, pHash])
+  const derivedKey = pbkdf2Sync(pbkdfPassword, nHash, 4096, 64, 'sha256')
+  const { privateKey, publicKey } = pki.ed25519.generateKeyPair({ seed: derivedKey })
+  return Buffer.concat([privateKey, publicKey])
 }
 
-export const createIdentityTicket = (name, pwd, challenge) => {
-  const keyPair = createKeyPair(name, pwd)
-  const solve = solveChallenge(challenge, keyPair.privateKey)
-  const ticket = {
-    name,
-    publicKey: keyPair.publicKey.toString('base64'),
-    challengeAnswer: solve
-  }
+// Converts a buffer key pair with concatenated public and private key to an object with a public and private key property.
+export const keyPairToObject = (keyPair) => {
+  const privateKey = Buffer.alloc(32)
+  const publicKey = Buffer.alloc(32)
 
-  const messageDigest = md.sha256.create().update(JSON.stringify(ticket), 'utf8').digest().getBytes().toString('base64')
+  keyPair.copy(privateKey, 0, 0, 32)
+  keyPair.copy(publicKey, 0, 32, 64)
+
+  return {
+    privateKey,
+    publicKey
+  }
+}
+
+// Creates an identity ticket for a given name, password and challenge.
+// Return format (bytes):
+// [Name length, 2 bytes][Name, n <= 65535 bytes][Public key, 32 bytes][Challenge answer, 64 bytes][Signature for preceding content, 64 bytes]
+// Length n + 162 bytes.
+export const createIdentityTicket = (name, pwd, challenge) => {
+  const keyPair = keyPairToObject(createKeyPair(name, pwd))
+  const solve = solveChallenge(challenge, keyPair.privateKey)
+  // Trim name to max 65535 bytes.
+  const nameBytes = Buffer.from(name, 'utf8').subarray(0, 65535)
+  // Get name length.
+  const nameLength = Buffer.alloc(2)
+  nameLength.writeUInt16LE(nameBytes.byteLength, 0)
+
+  // Sign ticket contents.
+  const unsignedTicket = Buffer.concat([nameLength, nameBytes, keyPair.publicKey, solve])
+  const ticketDigest = Buffer.from(md.sha256.create().update(unsignedTicket.toString('utf8'), 'utf8').digest().toHex(), 'hex')
 
   const signature = pki.ed25519.sign({
-    message: messageDigest,
-    encoding: 'utf8',
+    message: ticketDigest,
+    encoding: 'binary',
     privateKey: keyPair.privateKey
   })
 
-  ticket.signature = signature.toString('base64')
-  return ticket
+  return Buffer.concat([unsignedTicket, signature])
 }
 
+// Converts a buffer ticket with concatenated data to an object with a appropriate properties.
+export const ticketToObject = (ticket) => {
+  const nameLength = ticket.readUInt16LE(0)
+
+  const name = Buffer.alloc(nameLength)
+  const publicKey = Buffer.alloc(32)
+  const challengeAnswer = Buffer.alloc(64)
+  const signature = Buffer.alloc(64)
+
+  ticket.copy(name, 0, 2, 2 + nameLength)
+  ticket.copy(publicKey, 0, nameLength + 2, nameLength + 2 + 32)
+  ticket.copy(challengeAnswer, 0, nameLength + 34, nameLength + 34 + 64)
+  ticket.copy(signature, 0, nameLength + 98, nameLength + 98 + 64)
+
+  return {
+    nameLength,
+    name,
+    publicKey,
+    challengeAnswer,
+    signature
+  }
+}
+
+// Verifies an identity ticket by validating the signature.
 export const verifyIdentityTicket = (ticket, challengeQuestion) => {
   try {
-    const signature = Buffer.from(ticket.signature, 'base64')
-    delete ticket.signature
+    const parsedTicket = ticketToObject(ticket)
 
-    const messageDigest = md.sha256.create().update(JSON.stringify(ticket), 'utf8').digest().getBytes().toString('base64')
+    // Calculate hash.
+    const unsignedTicket = Buffer.alloc(parsedTicket.nameLength + 98)
+    ticket.copy(unsignedTicket, 0, 0, parsedTicket.nameLength + 98)
+    const messageDigest = Buffer.from(md.sha256.create().update(unsignedTicket.toString('utf8'), 'utf8').digest().toHex(), 'hex')
 
-    const challengeDigest = md.sha256.create().update(challengeQuestion, 'utf8').digest().getBytes().toString('base64')
-    const publicKey = Buffer.from(ticket.publicKey, 'base64')
+    const challengeDigest = Buffer.from(md.sha256.create().update(challengeQuestion.toString('utf8'), 'utf8').digest().toHex(), 'hex')
 
     const verifySign = pki.ed25519.verify({
       message: messageDigest,
-      encoding: 'utf8',
-      signature,
-      publicKey
+      encoding: 'binary',
+      signature: parsedTicket.signature,
+      publicKey: parsedTicket.publicKey
     })
 
     const verifyChallenge = pki.ed25519.verify({
       message: challengeDigest,
-      encoding: 'utf8',
-      signature: Buffer.from(ticket.challengeAnswer, 'base64'),
-      publicKey
+      encoding: 'binary',
+      signature: parsedTicket.challengeAnswer,
+      publicKey: parsedTicket.publicKey
     })
 
     return verifyChallenge && verifySign
   } catch {
+    console.log('Caught')
     return false
   }
 }
 
+// Solves a given challenge with a private key.
+// Return format (bytes):
+// [Challenge answer, 64 bytes]
+// Length 64 bytes.
 export const solveChallenge = (challenge, privateKey) => {
-  const messageDigest = md.sha256.create().update(challenge.toString('base64')).digest().getBytes().toString('base64')
+  const messageDigest = Buffer.from(md.sha256.create().update(challenge.toString('utf8'), 'utf8').digest().toHex(), 'hex')
   const answer = pki.ed25519.sign({
     message: messageDigest,
-    encoding: 'utf8',
+    encoding: 'binary',
     privateKey
   })
-  return answer.toString('base64')
+  return answer
+}
+
+// Generates a random challenge.
+export const generateRandomChallenge = (length = 512) => {
+  return Buffer.from(util.bytesToHex(random.getBytesSync(length)), 'hex')
 }
